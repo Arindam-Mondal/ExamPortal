cd76c80524dcf2b7ac7857627099ba464b6ad1e5
arindam <arindamm@mindfiresolutions.com>
2018-12-22 20:12:53 +0530

    Initial Commit

---
+++ examportal/src/test/java/com/examportal/app/ExamportalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.examportal.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamportalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
